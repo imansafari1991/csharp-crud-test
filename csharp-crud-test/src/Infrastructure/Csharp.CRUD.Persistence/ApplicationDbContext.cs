456f9381ccd01f418be4de10ab3029ad3272705f
iman.safari <iman.safari@gardenoflove.co>
2022-08-07 10:00:43 +0430

    add db context

---
+++ csharp-crud-test/src/Infrastructure/Csharp.CRUD.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Csharp.CRUD.Domain;
+
+namespace Csharp.CRUD.Persistence
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+
+    }
+}
