c60a7f08cc6b2d78f225e83fd8810f1d6d7802cd
iman.safari <iman.safari@gardenoflove.co>
2022-08-07 08:17:45 +0430

    add domain layer

---
+++ csharp-crud-test/src/core/Csharp.CRUD.Domain/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Csharp.CRUD.Domain;
+
+public class Customer
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateTime { get; set; }
+    public string PhoneNumber { get; set; }
+    [EmailAddress]
+    public string Email { get; set; }
+
+    public string BankAccountAddress { get; set; }
+}
